e52f83218c44b96a1aba7b38d023e7f950289e7e
andyvon <cd141@CD141.chengdu.com>
2015-03-23 14:36:34 +0800

    有用的js

---
+++ project/js/JsUtils.js
@@ -0,0 +1,31 @@
+﻿var JsUtils = {
+    /**排除字符数组中重复的成员*/
+    uniqueItem: function(arr) {
+        var res = [],
+        hash = {};
+        for (var i = 0,
+        elem; (elem = arr[i]) != null; i++) {
+            if (!hash[elem]) {
+                res.push(elem);
+                hash[elem] = true;
+            }
+        }
+        return res;
+    },
+    /**删除左右两端的空格*/
+    trim: function(str) {　　
+        return str.replace(/(^\s*)|(\s*$)/g, "");　　
+    },
+    /**删除左边的空格*/
+    ltrim: function(str) {　　
+        return str.replace(/(^\s*)/g, "");　　
+    },
+    /**删除右边的空格*/
+    rtrim: function (str) {　　
+        return str.replace(/(\s*$)/g, "");　　
+    },
+	replaceAll : function (str,srcChar,replaceText) {
+		var reg = RegExp(srcChar,'g');
+		return str.replace(reg,replaceText);
+	}
+}
